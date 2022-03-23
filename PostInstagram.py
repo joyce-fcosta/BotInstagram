eae1fcc52122b5c29d3d71e4f88346a0de49a011
Joyce Costa <72882311+joyce-fcosta@users.noreply.github.com>
2022-03-23 14:13:03 -0300

    Add files via upload
    Atualização

--- PostInstagram.py
+++ PostInstagram.py
@@ -6,24 +6,44 @@ import Login
 
 
 
-#with open("login.txt",'r') as lendo:
-#  Acess = lendo.read().split('\n')
+class BotInstagram(object):
 
-driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
-
-driver.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
-
-time.sleep(30)
-
-login = driver.find_element_by_name("username")
-login.click()
-
-time.sleep(4)
-
-for read in Login.name:
-    login.send_keys(read)
-    time.sleep(0.3)
+    driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
     
-senha = driver.find_element_by_name("password")
+    def __init__(self, site):
+        self.site = site
+
+    def Acess_Site(self):        
+        self.driver.get(self.site)
+        time.sleep(30)
+
+    def Click_Item(self,Key_Word):
+        login = self.driver.find_element_by_name(Key_Word)
+        login.click()
+        time.sleep(4)
+        return login
+
+        
+
+    def Typing_Site(self, string,Key_Word):        
+        for read in string:
+            self.Click_Item(Key_Word).send_keys(read)
+            time.sleep(0.1)
+    
+#senha = driver.find_element_by_name("password")
 
 
+
+def main():
+
+    Navegation = BotInstagram("https://www.instagram.com/accounts/login/?source=auth_switcher")
+
+    Navegation.Acess_Site()
+
+    Navegation.Click_Item("username")
+    Navegation.Typing_Site(Login.name,"username")
+    
+    Navegation.Click_Item("password")
+    Navegation.Typing_Site(Login.password,"password")
+
+main()
